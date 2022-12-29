c4c7c01b62615c96332833957b7327574b49d402
David Barrat <david@barrat.io>
2022-12-29 17:32:39 +0100

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,29 @@
 module.exports = {
-    moduleFileExtensions: ["ts", "tsx", "js"],
-    transform: {
-      "^.+\\.(ts|tsx)?$": ['ts-jest', { "tsConfig": "<rootDir>/tsconfig.base.json" }]
-    },
-    testMatch: ["**/test/**/*.test.(ts|js)", "**/**/*.test.(ts|js)"],
-    testPathIgnorePatterns: ["/node_modules/", "build"],
-    testEnvironment: "node",
-    rootDir: ".",
-    collectCoverageFrom: [
-      "!<rootDir>/src",
-    ]
-  };
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'tsx',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)?$': [
+      'ts-jest',
+      {
+        tsConfig: '<rootDir>/tsconfig.base.json',
+      },
+    ],
+  },
+  testMatch: [
+    '**/**/*.test.(ts|js)',
+    '**/test/**/*.test.(ts|js)',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'build',
+  ],
+  testEnvironment: 'node',
+  rootDir: '.',
+  collectCoverageFrom: [
+    '!<rootDir>/src',
+  ],
+  preset: 'ts-jest',
+}
